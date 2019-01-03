273e2f63c722af02dd273e498bedf54754fad274
CaityHeath <caity.heath@gmail.com>
2019-01-02 17:03:29 -0800

    refactored starter code to use promises and modularized the events

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
+const events = require('./events.js');
+//const logger = require('./logger.js');
+
+
+let read = util.promisify(fs.readFile);
+let write = util.promisify(fs.writeFile);
+let file = process.argv.slice(2).shift();
+
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
+  read(file)
+    .then( (data) => {
+      let text = data.toString().toUpperCase();
+      console.log(text);
+      write(file, Buffer.from(text))
+        .then(() => {
+          console.log(file);
+          console.log(`${file} saved`);
+        })
+        .catch(error => events.emit('error'));
+    })
+    .catch(error => events.emit('error'));
 };
 
-let file = process.argv.slice(2).shift();
 alterFile(file);
